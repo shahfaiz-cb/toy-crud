851979fd057238cf3a4cadf57d077e2a5e939284
Faizaan Hussain <faizaan.hussain@couchbase.com>
2026-02-18 11:17:30 +0530

    feat(ui): implement authentication context and provider with token management

---
+++ capella_ui/src/hooks/use-auth/use-auth.ts
@@ -0,0 +1,11 @@
+import { AuthContext } from "contexts/auth-context";
+import { useContext } from "react";
+
+export function useAuth() {
+    const context = useContext(AuthContext)
+
+    if(!context) {
+        throw new Error("useAuth must be used within AuthProvider")
+    }
+    return context
+}
--- capella_ui/src/layouts/auth-layout/auth-layout.tsx
+++ capella_ui/src/layouts/auth-layout/auth-layout.tsx
@@ -1,6 +1,12 @@
-import { ReactNode } from "react";
-import { Outlet } from "react-router-dom";
+import { useAuth } from "hooks/use-auth";
+import { Navigate, Outlet } from "react-router-dom";
 export function AuthLayout() {
+    const { token } = useAuth()
+
+    if(token) {
+        return <Navigate to={"/dashboard"} replace/>
+    }
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-slate-50 to-slate-200 p-4">
             <div className="w-full max-w-md">
