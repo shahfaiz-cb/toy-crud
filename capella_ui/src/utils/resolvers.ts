57dd828aece4628805f676dcde6a145ad9d2da0a
Faizaan Hussain <faizaan.hussain@couchbase.com>
2026-02-19 00:13:49 +0530

    fix(ui): remove unused priority encoding function from resolvers

--- capella_ui/src/utils/resolvers.ts
+++ capella_ui/src/utils/resolvers.ts
@@ -2,12 +2,3 @@ export function priorityResolver(priority: number) {
     const priorities = ["Low", "Medium", "High", "Urgent"]
     return priorities[priority]
 }
-
-export function priorityEncode(priority: string) {
-    switch (priority) {
-        case "URGENT": return 3
-        case "HIGH": return 2
-        case "MEDIUM": return 1
-    }
-    return 0
-}
